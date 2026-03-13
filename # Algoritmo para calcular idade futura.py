9dd617c6f523acbc8f3e62d3212a3eea5433e042
Iasmim Mann <MANNIASMIM@GMAIL.COM>
2026-03-13 04:48:43 -0300

    Algoritmo para calcular idade futura
    Desafio: Problema 1—Cálculo de idade futura:
    
    Construa um algoritmo que:
    
    1. Receba o nome de uma pessoa;
    2. Receba sua idade atual;
    3. Calcule qual será sua idade daqui a 10 anos;
    4. Exiba o nome da pessoa e a idade futura.
    
    Exemplo de saída esperada:
    
    João terá 35 anos daqui a 10 anos.
    
    //////////

---
+++ # Algoritmo para calcular idade futura.py
@@ -0,0 +1,8 @@
+#Algoritmo para calcular idade futura
+
+nome = input("Digite o nome da pessoa: ")
+idade_atual = int(input("Digite a idade atual: "))
+
+idade_futura = idade_atual + 10
+
+print(f"{nome} terá {idade_futura} anos daqui a 10 anos.")
